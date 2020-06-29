3eea338ca340abe379e8f0d4bc24d58631cf8360
EnselerizeR <enselerizer@gmail.com>
2020-06-29 16:17:33 +0300

    Angular-electron app setup, basic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: MainPageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LeftMenuComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
